378acff49a9144b29f3416375db805128d6a0e0e
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-05-16 16:16:06 -0700

    (refactor) Refactor API routing to use in-memory DB.

--- app/server/server.js
+++ app/server/server.js
@@ -2,33 +2,46 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var express = require('express');
 
-var indexRouter = require('./routes/index');
 
-// var db = { };
-// var mockEmployee1 = {
-//   employerId: 1,
-//   name: 'Jane Doe',
-//   phoneNumber: '(555) 555-0001',
-//   email: 'jane.doe@example.com'
-// };
-// var mockEmployee2 = {
-//   employerId: 2,
-//   name: 'Jane Doe\'s Sister',
-//   phoneNumber: '(555) 555-0002',
-//   email: 'janes.sister@example.com'
-// };
-// var mockEmployee3 = {
-//   employerId: 3,
-//   name: 'Jane Doe\'s Brother',
-//   phoneNumber: '(555) 555-0003',
-//   email: 'janes.brother@example.com'
-// };
-// db.employees = [
-//   mockEmployee1,
-//   mockEmployee2,
-//   mockEmployee3
-// ];
 
+var db = { employees: [] };
+// BEGIN MOCK DATA
+var mockEmployee1 = {
+  employerId: 1,
+  name: 'Jane Doe',
+  phoneNumber: '(555) 555-0001',
+  email: 'jane.doe@example.com'
+};
+var mockEmployee2 = {
+  employerId: 2,
+  name: 'Jane Doe\'s Sister',
+  phoneNumber: '(555) 555-0002',
+  email: 'janes.sister@example.com'
+};
+var mockEmployee3 = {
+  employerId: 3,
+  name: 'Jane Doe\'s Brother',
+  phoneNumber: '(555) 555-0003',
+  email: 'janes.brother@example.com'
+};
+db.employees = [
+  mockEmployee1,
+  mockEmployee2,
+  mockEmployee3
+];
+// END MOCK DATA
+
+
+
+// SET UP ROUTING
+var indexRouter = express.Router();
+indexRouter.get('/', function (req, res, next) {
+  res.status(200).send(db);
+});
+
+
+
+// SET UP SERVER
 var server = express();
 
 // Server config:
